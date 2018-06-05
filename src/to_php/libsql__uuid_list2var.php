4d488136468469c09147858712b2fd5e13d8d23a
progman <gnuplanet@gmail.com>
2018-06-05 17:08:16 +0300

    uuid is not replace by default now and add libsql__uuidx2var() and libsql__uuidx_list2var()

--- src/to_php/libsql__uuid_list2var.php
+++ src/to_php/libsql__uuid_list2var.php
@@ -3,9 +3,9 @@ function libsql__uuid_list2var($table_name_short, $col_name, $flag_make_col_alia
 {
 	if ($flag_make_col_alias === false)
 	{
-		return "UUID_LIST2JSON(".$table_name_short.".".$col_name.")::json";
+		return "TO_JSON(".$table_name_short.".".$col_name.")::json";
 	}
 
-	return "UUID_LIST2JSON(".$table_name_short.".".$col_name.")::json AS ".$col_name;
+	return "TO_JSON(".$table_name_short.".".$col_name.")::json AS ".$col_name;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
